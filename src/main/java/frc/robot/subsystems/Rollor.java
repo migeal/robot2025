e9c18061448764ba7bc403995f341e632fd8f66a
4919frcteam <127057006+4919frcteam@users.noreply.github.com>
2025-02-02 13:09:20 -0800

    The intake and rotation of the intake.

---
+++ src/main/java/frc/robot/subsystems/Rollor.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+  
+import com.revrobotics.Spark.SparkMax;
+import com.revrobotics.RelativeEncoder;
+
+public class Roller extends SubsystemBase {
+public Roller(){}
+private spark intake = new Spark(motorConstants.InmotorL);
+
+
+//grabing the algee and coral 
+public void grab(){
+    intake.set(0.5);
+}
+//holding algee and coral
+@Override
+public void hold(){
+    intake.set(0)
+}
+//relaseing algee 
+public void release(){
+    intake.set(-0.5) 
+}
+@Override
+   public void periodic() {
+     // This method will be called once per scheduler run
+   }
+ 
+   @Override
+   public void simulationPeriodic() {
+     // This method will be called once per scheduler run during simulation
+   }
+}
+
