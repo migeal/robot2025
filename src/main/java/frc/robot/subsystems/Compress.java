eb222985d0dd720f2b0bf5aeea91afb3fdae5d47
4919frcteam <127057006+4919frcteam@users.noreply.github.com>
2025-03-16 13:17:55 -0700

    supply compressor

--- src/main/java/frc/robot/subsystems/Compress.java
+++ src/main/java/frc/robot/subsystems/Compress.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Compress extends SubsystemBase {
 Compress(){}
-//private final compressor = new compressor;
+private final Compressor supply = new Compressor(PneumaticsModuleType.CTREPCM);
 }
