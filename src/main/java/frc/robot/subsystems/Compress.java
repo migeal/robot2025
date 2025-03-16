b8311961b9c47ae2fff86825b3c9ae69ee9cea28
4919frcteam <127057006+4919frcteam@users.noreply.github.com>
2025-03-16 13:10:21 -0700

    non error false compressor

--- src/main/java/frc/robot/subsystems/Compress.java
+++ src/main/java/frc/robot/subsystems/Compress.java
@@ -1,4 +1,5 @@
-
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Compress extends SubsystemBase {
 Compress(){}
 //private final compressor = new compressor;
